295fa01084903e04e4ef7e349042610061ed0774
Matt Goldenberg <magicmattg@gmail.com>
2015-01-07 10:40:44 -0800

    Added tests for multiply and power

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -78,15 +78,18 @@ end
 # write tests and code for the following:
 
 describe "#multiply" do
-
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    multiply([2, 10]).should == 20;
+  end
 
   it "multiplies several numbers"
-
+    multiply([2,10,5]).should == 100;
+  end
 end
 
 describe "#power" do
   it "raises one number to the power of another number"
+    power(5,6).should == 15625;
 end
 
 # http://en.wikipedia.org/wiki/Factorial
